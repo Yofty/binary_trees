7a185f6633fb10e4cfd1e4cebb0ee205fbf52c53
Yofty <106748943+Yofty@users.noreply.github.com>
2022-12-15 04:37:14 -0800

    space correction

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,14 +1,14 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_root -  checks if a given node is a root
+ * binary_tree_is_root - checks if a given node is a root
  * @node: the node to be checked
  *
  * Return: int 0 or 1
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node == NULL ||node->parent != NULL)
+	if (node == NULL || node->parent != NULL)
 		return (0);
 	return (1);
 }
